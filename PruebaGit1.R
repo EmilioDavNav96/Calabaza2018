ada5eb0f1d4cd9c39ff6c6ef23b3309538338b78
Emilio <dav.nav96@gmail.com>
2019-01-22 23:01:51 -0600

    Añadi archivos nuevos

---
+++ PruebaGit1.R
@@ -0,0 +1,2 @@
+nombre<-c("Hermano", "Listo")
+nombre
---
+++ PruebasEstadísticas.R
@@ -0,0 +1,16 @@
+#Pruebas Estadísticas#
+
+rm(list=ls())
+
+install.packages("lsmeans")
+
+#Cargar librerías
+library(dplyr)
+library(tidyr)
+library(multcompView)
+library(stringr)
+library(Hmisc)
+library(lsmeans)
+
+
+<-read.csv()
